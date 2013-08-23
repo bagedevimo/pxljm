c59f7908f59d30e264b836f728aa845d397754d6
Ben Allen <benos.9k@gmail.com>
2013-08-23 21:12:50 +1200

    lib loading

--- eclipse/src/goldeneagle/Main.java
+++ eclipse/src/goldeneagle/Main.java
@@ -1,8 +1,19 @@
 package goldeneagle;
 
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+
 public class Main {
 
+	private static void loadLibs() {
+		File libpath = new File("./lib/native/" + LWJGLUtil.getPlatformName());
+		System.setProperty("org.lwjgl.librarypath", libpath.getAbsolutePath());
+	}
+	
 	public static void main(String[] args) {
+		loadLibs();
+		
 		PxlGame game = new PxlGame();
 		
 		boolean fullscreen = false;
