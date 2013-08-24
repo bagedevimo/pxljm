ac3602e9fe65aba840101d4448350ee52ef2f5e3
Ben Anderson <benanderson@acidic.co.nz>
2013-08-24 22:47:18 +1200

    Added walking animations, added better tile loading

--- eclipse/src/goldeneagle/entities/PlayerEntity.java
+++ eclipse/src/goldeneagle/entities/PlayerEntity.java
@@ -7,31 +7,115 @@ import org.lwjgl.input.Keyboard;
 import goldeneagle.Frame;
 import goldeneagle.ResourceCache;
 import goldeneagle.Vec3;
+import goldeneagle.clock.DerivedClock;
 import goldeneagle.scene.Entity;
 import goldeneagle.scene.SceneManager;
+import goldeneagle.util.Profiler;
 
 public class PlayerEntity extends Entity {
+	private static final int PlayerDraw = Profiler.createSection("PlayerDraw");
+	
+	
+
+	int nFrames = 21;
+	double frameWidth = 1.0f/nFrames;
+
+	DerivedClock animationClock;
+	double animationStart;
+	double walkFrameTime = 1/20f;
+	double runFrameTime = 1/30f;
+	
+	double lastAttrUpdate;
+	
+	boolean isMoving = false;
+	boolean isRunning = false;
+
+	double MaxHealth = 100;
+	double Health = MaxHealth;
+	
+	double MaxNutrition = 100;
+	double Nutrition = MaxNutrition;
+	double DefaultHunger = 0.01;
+	double Hunger = DefaultHunger;
+	
+	double MaxHydration = 100;
+	double Hydration = MaxHydration;
+	double DefaultThirst = 0.02;
+	double Thirst = DefaultThirst;
+	
+	double MaxEnergy = 100;
+	double Energy = MaxEnergy;
+	
+	double NormalTemp = 37.0f;
+	double Temp = NormalTemp;
 	
 	public PlayerEntity(Frame parent_) {
 		super(parent_);
+		
+		animationClock = new DerivedClock(this.getClock(), 0);
+		animationStart = this.animationClock.get();
+		
+		this.lastAttrUpdate = this.getClock().get();
+	}
+	
+	private double getFrameTime() {
+		if(this.isRunning)
+			return runFrameTime;
+		return walkFrameTime;
 	}
 
 	@Override
 	public void Draw() {				
-		glBegin(GL_POLYGON);
+		int texID = -1;
+		
+		
+		
+		double dT = (this.animationClock.get() - this.animationStart);
+		int frame = (int)(dT / getFrameTime());
+		if(frame >= nFrames) {
+			this.animationStart = this.animationClock.get();
+			dT = (this.animationClock.get() - this.animationStart);
+			frame = (int)(dT / getFrameTime());
+		}		
+		
+		try {
+			texID = ResourceCache.GetGLTexture("./assets/sprites/character_walk.png");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-		glBindTexture(GL_TEXTURE_2D, 0);
-		glColor3d(1, 0, 0);
+ 		glBindTexture(GL_TEXTURE_2D, texID);
+		glEnable(GL_TEXTURE_2D);
+		
+		glEnable(GL_ALPHA_TEST);
+		glAlphaFunc(GL_GREATER, 0.5f);
+		
+		Profiler.enter(PlayerDraw);
+		
+		glBegin(GL_POLYGON);
+		
+		glColor3d(1, 1, 1);
 		glNormal3d(0, 0, 1);
 		
-		glVertex3d(-0.5, -0.5, SceneManager.Z_PLAYER);
-		glVertex3d(0.5, -0.5, SceneManager.Z_PLAYER);
-		glVertex3d(0.5, 0.5, SceneManager.Z_PLAYER);
-		glVertex3d(-0.5, 0.5, SceneManager.Z_PLAYER);
+		double u = frameWidth*frame;
+		
+		double size = 1.8f;
+		glTexCoord2d(u, 0);
+		glVertex3d(-size, -size, SceneManager.Z_PLAYER);
+		glTexCoord2d(u+frameWidth, 0);
+		glVertex3d(size, -size, SceneManager.Z_PLAYER);
+		glTexCoord2d(u+frameWidth, 1);
+		glVertex3d(size, size, SceneManager.Z_PLAYER);
+		glTexCoord2d(u, 1);
+		glVertex3d(-size, size, SceneManager.Z_PLAYER);
 		
 		glEnd();
 		
+		Profiler.exit(PlayerDraw);
+		
 		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_ALPHA_TEST);
 	}
 	
 	@Override
@@ -50,7 +134,11 @@ public class PlayerEntity extends Entity {
 		double speed = 3.5 * deltaTime;
 		
 		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
-			speed *= 8;
+		{
+			isRunning = true;
+			speed *= 2;
+		} else
+			isRunning = false;
 		
 		double x = 0, y = 0;
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
@@ -64,9 +152,38 @@ public class PlayerEntity extends Entity {
 			
 		
 		motion = this.getPosition().add(new Vec3(x, y, 0));
+		if(x == 0 && y == 0) {
+			this.animationClock.pause();
+			this.isMoving = false;
+		}
+		else {
+			this.animationClock.play();
+			this.isMoving = true;
+		}
+			
 		
 		this.setLinear(motion, Vec3.zero);
-			
+		
+		if(this.getClock().get() - this.lastAttrUpdate > 1)
+			this.updateAttrs();
+	}
+	
+	private void updateAttrs() {
+		this.lastAttrUpdate = this.getClock().get();
+		
+		this.Nutrition -= this.Hunger;
+		this.Hydration -= this.Thirst;
+		
+		if(this.isRunning && this.Thirst < 0.03f)
+			this.Thirst += 0.0001;
+		else if(this.Thirst > this.DefaultThirst)
+			this.Thirst -= 0.0001;
+		
+		System.out.printf("Energy: %f\tdEnergy: %f\n", this.Energy, 0f);
+		System.out.printf("Nutrition: %f\tdNutrition: %f\n", this.Nutrition, this.Hunger);
+		System.out.printf("Hydration: %f\tddHydration: %f\n", this.Hydration, this.Thirst);
+		System.out.printf("Health: %f\tdHealth: %f\n",  this.Health, 0f);
+		System.out.printf("Temperature: %f\tdTemperature: %f\n", this.Temp, 0f);
 	}
 
 }
