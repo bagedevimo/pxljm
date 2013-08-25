4129da6a4e31fe17ad2f4a78ad8b3211806b746a
Ben Anderson <benanderson@acidic.co.nz>
2013-08-25 18:22:07 +1200

    Fixed pickup drawing

--- eclipse/src/goldeneagle/entities/PickupEntity.java
+++ eclipse/src/goldeneagle/entities/PickupEntity.java
@@ -1,6 +1,8 @@
 package goldeneagle.entities;
 
 import static org.lwjgl.opengl.GL11.GL_ALPHA_TEST;
+import static org.lwjgl.opengl.GL11.GL_AMBIENT_AND_DIFFUSE;
+import static org.lwjgl.opengl.GL11.GL_FRONT;
 import static org.lwjgl.opengl.GL11.GL_GREATER;
 import static org.lwjgl.opengl.GL11.GL_POLYGON;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
@@ -11,6 +13,7 @@ import static org.lwjgl.opengl.GL11.glColor3d;
 import static org.lwjgl.opengl.GL11.glDisable;
 import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glMaterial;
 import static org.lwjgl.opengl.GL11.glNormal3d;
 import static org.lwjgl.opengl.GL11.glTexCoord2d;
 import static org.lwjgl.opengl.GL11.glVertex3d;
@@ -26,13 +29,14 @@ import goldeneagle.items.Item;
 import goldeneagle.scene.Frame;
 import goldeneagle.scene.Entity;
 import goldeneagle.scene.Scene;
+import goldeneagle.scene.SceneManager;
 import goldeneagle.scene.ShadowCaster;
 import goldeneagle.state.Collidable;
 import goldeneagle.util.Profiler;
 
 public class PickupEntity extends Entity implements Collidable{
 
-	private static final int size = 32;
+	private static final double size = 1.4f;
 	private final Item item;
 	private Bound bound; 
 	
@@ -40,7 +44,9 @@ public class PickupEntity extends Entity implements Collidable{
 		super(parent_);
 		item = item_;
 		bound = new BoundingSphere(this, size);
-		setHeight(1);
+		
+		this.setLinear(new Vec3(xPos_, yPos_, 0), Vec3.zero);
+		this.setHeight(0.3);
 		
 	}
 	
@@ -70,29 +76,29 @@ public class PickupEntity extends Entity implements Collidable{
 			e.printStackTrace();
 		}
 	
- 		glBindTexture(GL_TEXTURE_2D, texID);
+		glBindTexture(GL_TEXTURE_2D, texID);
 		glEnable(GL_TEXTURE_2D);
 		
 		glEnable(GL_ALPHA_TEST);
 		glAlphaFunc(GL_GREATER, 0.5f);
 
+		glMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, SceneManager.floatv(1, 1, 1, 1));
+		
 		glBegin(GL_POLYGON);
-		
-		glColor3d(1, 1, 1);
 		glNormal3d(0, 0, 1);
-		
 		glTexCoord2d(0, 0);
-		glVertex3d(-size, -size, 0);
+		glVertex3d(-this.size, -this.size, 0);
 		glTexCoord2d(1, 0);
-		glVertex3d(size, -size, 0);
+		glVertex3d(this.size, -this.size, 0);
 		glTexCoord2d(1, 1);
-		glVertex3d(size, size, 0);
+		glVertex3d(this.size, this.size, 0);
 		glTexCoord2d(0, 1);
-		glVertex3d(-size, size, 0);
+		glVertex3d(-this.size, this.size, 0);
 		
 		glEnd();	
-
+		
 		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_ALPHA_TEST);
 	}
 
 	@Override
